110c31842adb00bbabdc24afb81577269f4423b2
Thomas Zwagerman <thozwa@sci1.jasmin.ac.uk>
2024-06-18 14:58:51 +0100

    updating pipeline to work with envs

--- run_asli_pipeline.sh
+++ run_asli_pipeline.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Read in config file
-. ENVS
+source ENVS
 
 # Location that pipeline is stored, referenced by most scripts
 PIPELINE_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -13,8 +13,13 @@ DATE=$(date --utc +"%Y-%m-%d")
 # Activative environment
 source ${ASLI_VENV}
 
+# Create input and output directories, if they do not already exist
 if [ ! -d "$DATA_DIR" ]; then
-  mkdir $DATA_DIR
+  mkdir -p $DATA_DIR
+fi
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p $OUTPUT_DIR
 fi
 
 # Fetch land sea mask, automatically writes in data directory
@@ -26,11 +31,11 @@ asli_data_era5 $DATA_ARGS_ERA5
 
 # Run calculation, specifying output location
 # output.csv will need to be renamed to sensible unique identifyer 
-asli_calc ERA5/monthly/era5_mean_sea_level_pressure_monthly_*.nc -o /gws/nopw/j04/dit/users/thozwa/output_csv/output.csv
+asli_calc $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -o $OUTPUT_DIR/output.csv
 # probably move into sbatch to run on lotus - not strictly required but nice for reproducibility
 
 # Move output into s3 bucket, making sure /.s3cfg file is present
-s3cmd put output_csv/output.csv s3://asli
+s3cmd put $OUTPUT_DIR/output.csv s3://asli
 
 # We can then move ERA5 data onto the object store as well OR
 # Check if this can be done from the CEDA archive
