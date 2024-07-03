4d06e06328c2880c40d162db6ab80104ca8e4f41
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2024-07-03 11:19:55 +0100

    remove directory setup script

--- run_asli_pipeline.sh
+++ run_asli_pipeline.sh
@@ -4,18 +4,34 @@ set -e
 # Read in config file
 source ENVS
 
-./01_set_up_directories.sh
+# Location that pipeline is stored
+PIPELINE_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-./02_run_asli_calculations.sh
+# Activate virtual environment
+source ${ASLI_VENV}
+
+# Put all relevant directories in a list
+DIR_LIST=($DATA_DIR $OUTPUT_DIR)
+
+# Create them if they do not exist
+for DIR in ${DIR_LIST[@]};
+do
+	if [ ! -d $DIR ]; then
+  		mkdir -p $DIR
+		echo "Created $DIR"
+	fi
+done
+
+bash 01_run_asli_calculations.sh
 
 # Exports files to destination, either object storage of classic file system
 # This also determines the file export format
 case ${FILE_DESTINATION} in
 	OBJECT_STORAGE|BOTH)
-		./03_export_to_object_store.sh
+		bash 02_export_to_object_store.sh
 		;;&
 	FILE_SYSTEM|BOTH)
-		./04_export_to_file_system.sh
+		bash 03_export_to_file_system.sh
 		;;
 	*)
 	echo "ERROR: This is not a valid destination, choose from: ${VALID_DESTINATIONS[@]}"
