890f8cebc585eb335860b9e00b15b26d10b6aae5
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2024-07-03 17:06:58 +0100

    test msm-os

--- 02_export_to_object_store.sh
+++ 02_export_to_object_store.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-# Convert csv to parquet placeholder
-#duckdb -c "COPY (SELECT * FROM read_csv_auto('$OUTPUT_DIR')) TO '${$OUTPUT_DIR}/output.parquet' (FORMAT PARQUET);" 
+# Get file list
+file_list=$(find $OUTPUT_DIR -maxdepth 2 -type f -name "*.nc" | sort)
 
-s3cmd put $OUTPUT_DIR/output.csv $S3_BUCKET
-echo "Writing to Object Storage, bucket $S3_BUCKET."
+# Check if OUTPUT directory exists and that it contains NetCDF files
+if [ ! -d "OUTPUT" ] || [ ! -z "${file_list}" ]; then
+    echo "Error: No .nc files found in OUTPUT directory or OUTPUT directory does not exist."
+    exit 1
+fi
+
+total_files=$(echo "$file_list" | wc -l)
+
+counter=0
+for file in $file_list; do
+    ((counter++))
+    percentage=$((counter * 100 / total_files))
+
+    echo -ne "Progress: ["
+    for ((i = 0; i < percentage / 2; i++)); do
+        echo -ne "="
+    done
+    echo -ne ">] $percentage% \r"
+
+    echo -e "Sending ${file}"
+    msm_os send -f ${file} -c ${S3_CREDENTIALS} -b ${S3_BUCKET}
+done
+
+echo -e "\nTransfer completed!"
--- 03_export_to_file_system.sh
+++ 03_export_to_file_system.sh
@@ -2,5 +2,6 @@
 
 # Rsync the contents of $OUTPUT_DIR to $RSYNC_LOCATION
 # In this case there is no need to convert file formats
-rsync $OUTPUT_DIR/ $RSYNC_LOCATION
+# But we will specify csv files only
+rsync $OUTPUT_DIR/*.csv $RSYNC_LOCATION
 echo "Writing to file system, folder $RSYNC_LOCATION."
