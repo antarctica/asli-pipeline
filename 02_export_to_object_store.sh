4b39a7a8cdb2144c344e426dd07b3c48ed01672b
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2024-07-03 12:27:29 +0100

    remove placeholder parquet code

--- 02_export_to_object_store.sh
+++ 02_export_to_object_store.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Convert csv to parquet placeholder
-duckdb -c "COPY (SELECT * FROM read_csv_auto('$OUTPUT_DIR')) TO '${$OUTPUT_DIR}/output.parquet' (FORMAT PARQUET);" 
+#duckdb -c "COPY (SELECT * FROM read_csv_auto('$OUTPUT_DIR')) TO '${$OUTPUT_DIR}/output.parquet' (FORMAT PARQUET);" 
 
-s3cmd put $OUTPUT_DIR/output.parquet $S3_BUCKET
+s3cmd put $OUTPUT_DIR/output.csv $S3_BUCKET
 echo "Writing to Object Storage, bucket $S3_BUCKET."
