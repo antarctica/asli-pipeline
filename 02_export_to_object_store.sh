a69d9bd193038246969a4fab402422a8ebf68114
thomaszwagerman <thozwa@bas.ac.uk>
2024-07-17 15:56:42 +0100

    add py script to chunk nc to zarr and send to s3

--- 02_export_to_object_store.sh
+++ 02_export_to_object_store.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Move ERA5 monthly files to $OUTPUT_DIR
-scp $DATA_DIR/*.nc $OUTPUT_DIR
-
 # Check if OUTPUT directory exists and that it contains files
 if [ ! -d $OUTPUT_DIR ]; then
     echo "Error: OUTPUT directory does not exist."
@@ -21,17 +18,14 @@ for file in $OUTPUT_DIR/*; do
         echo -ne "="
     done
     echo -ne ">] $percentage% \r"
-
     echo -e "Sending ${file}"
-    if [[ $file == *.nc ]]
-    then
-        msm_os send -f ${file} -c ${S3_CREDENTIALS} -b ${S3_BUCKET_ZARR}
-        echo -e "\n Transfer to $S3_BUCKET_ZARR completed!"
-    else 
-        if [[ $file == *.csv ]]
-        then
-            s3cmd put ${file} $S3_BUCKET_CSV
-            echo -e "\n Transfer to $S3_BUCKET_CSV completed!"
-        fi
-    fi
+
+    # Simply puts files in object store
+    # Credentials handled via .s3cfg
+    s3cmd put ${file} $S3_BUCKET_CSV
+    echo -e "\n Transfer to $S3_BUCKET_CSV completed!"
 done
+
+# Now also transfer ERA5 to object store as zarr files
+python export_nc_as_zarr.py "$DATA_DIR/*.nc" "$S3_BUCKET_ZARR/zarr-files"
+echo -e "\n Transfer to $S3_BUCKET_ZARR completed!"
