0edfb469c6f1def881e3e601053c56bec3c19dbb
thomaszwagerman <thozwa@bas.ac.uk>
2024-07-31 15:40:04 +0100

    remove example variables

--- 01_run_asli_calculations.sh
+++ 01_run_asli_calculations.sh
@@ -7,9 +7,6 @@ asli_data_lsm
 # Downloading latest ERA5 data
 asli_data_era5 $DATA_ARGS_ERA5
 
-# Second file for testing
-asli_data_era5 $DATA_ARGS_ERA5_TEST
-
 # Run calculation, specifying output location
 # output.csv will need to be renamed to sensible unique identifyer 
 asli_calc $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -o $OUTPUT_DIR/asli_calculation_$DATE.csv
