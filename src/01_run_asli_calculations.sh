582119a2c596bac4d2c90f3ba87fe4ab15e0648a
thomaszwagerman <thozwa@bas.ac.uk>
2024-11-25 11:25:53 +0000

    adding configuration for njobs to parallelise calculations

--- src/01_run_asli_calculations.sh
+++ src/01_run_asli_calculations.sh
@@ -3,4 +3,4 @@ set -e
 
 # Run calculation, specifying output location
 # output.csv will need to be renamed to sensible unique identifier 
-asli_calc $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -o $OUTPUT_DIR/asli_calculation_$FILE_IDENTIFIER.csv
+asli_calc $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -o $OUTPUT_DIR/asli_calculation_$FILE_IDENTIFIER.csv -n $NJOBS_CALC
