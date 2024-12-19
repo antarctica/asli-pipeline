d7bc7c89f15435be73d025732c62ca97e352fabc
thomaszwagerman <thozwa@bas.ac.uk>
2024-12-19 16:00:57 +0000

    adding arguments to arrow script

--- src/write_feather.py
+++ src/write_feather.py
@@ -5,6 +5,6 @@ import sys
 # usage python write_feather.py $OUTPUT_DIR $FILE_IDENTIFIER
 
 # Reading in csv and writing as feather
-df_asli = pd.read_table(sys.argv[1] + "asli_calculation_" + sys.argv[2] + ".csv")
+df_asli = pd.read_table(sys.argv[1] + "/asli_calculation_" + sys.argv[2] + ".csv")
 
 feather.write_feather(df_asli, sys.argv[1] + "feather/asli_calculation_" + sys.argv[2])
