f65463e8584062c14217dec0adccd427e40f2831
thomaszwagerman <thozwa@bas.ac.uk>
2025-02-05 11:05:40 +0000

    iterate over years specified when plotting

--- src/01_run_asli_calculations.sh
+++ src/01_run_asli_calculations.sh
@@ -8,5 +8,7 @@ asli_calc $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -o $OUTPUT_DIR/asl
 
 # If OUTPUT_PLOTTING is set to true also output plots
 if [[ "${OUTPUT_PLOTTING}" == true ]]; then
-    asli_plot $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -i $OUTPUT_DIR/asli_calculation_$FILE_IDENTIFIER.csv -o $OUTPUT_DIR/asli_plot_$FILE_IDENTIFIER.png
+    for plot_year in $(seq $START_YEAR $END_YEAR);
+        do asli_plot $DATA_DIR/era5_mean_sea_level_pressure_monthly_*.nc -y $plot_year -i $OUTPUT_DIR/asli_calculation_$FILE_IDENTIFIER.csv -o $OUTPUT_DIR/asli_plot_$plot_year.png;
+    done
 fi
