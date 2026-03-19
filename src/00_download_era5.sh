023fd6080d2e53196bd6275f66a74459a81df96e
davidwilby <24752124+davidwilby@users.noreply.github.com>
2026-03-19 15:06:59 +0000

    reorganise date variables; set -u in data download script to catch any unset variables

--- src/00_download_era5.sh
+++ src/00_download_era5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 
 # Read in config file
 source ENVS
@@ -21,11 +21,12 @@ done
 
 # Fetch land sea mask, automatically writes in data directory
 # Everything is pre-set in asli functions, no arguments needed for our purpose
+echo "Requesting land sea mask data"
 asli_data_lsm
 
 # Downloading latest ERA5 data, provide information to the user
-echo "Requesting with the following arguments: $DATA_ARGS_ERA5".
-asli_data_era5 $DATA_ARGS_ERA5
+echo "Requesting data from $START_YEAR to $END_YEAR".
+asli_data_era5 -s $START_YEAR -n $END_YEAR
 
 # The newer CDS API may return zip archives even when netcdf format is requested.
 # Extract any downloaded .nc files that are actually zip archives.
@@ -45,3 +46,4 @@ for f in "$DATA_DIR"/era5_mean_sea_level_pressure_monthly_*.nc; do
 		rm -rf "$tmpdir"
 	fi
 done
+
